d32265452b0c20c2e112a6e52bb1510679c927c9
Sylvain Henry <sylvain@haskus.fr>
2020-06-15 15:09:11 +0200

    DynFlags: add OptCoercionOpts
    Use OptCoercionOpts to avoid threading DynFlags all the way down to
    GHC.Core.Coercion.Opt

--- compiler/GHC/Core/Opt/Simplify.hs
+++ compiler/GHC/Core/Opt/Simplify.hs
@@ -1133,8 +1133,8 @@ simplCoercionF env co cont
 
 simplCoercion :: SimplEnv -> InCoercion -> SimplM OutCoercion
 simplCoercion env co
-  = do { dflags <- getDynFlags
-       ; let opt_co = optCoercion dflags (getTCvSubst env) co
+  = do { opts <- getOptCoercionOpts
+       ; let opt_co = optCoercion opts (getTCvSubst env) co
        ; seqCo opt_co `seq` return opt_co }
 
 -----------------------------------
