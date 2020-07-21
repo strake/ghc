fad4df7324dab51f764c542662e34d99a444693b
Sylvain Henry <sylvain@haskus.fr>
2020-07-21 19:15:59 +0200

    Move GHC.Platform into the compiler
    Previously it was in ghc-boot so that ghc-pkg could use it. However it
    wasn't necessary because ghc-pkg only uses a subset of it: reading
    target arch and OS from the settings file. This is now done via
    GHC.Platform.ArchOS (was called PlatformMini before).

--- compiler/GHC.hs
+++ compiler/GHC.hs
@@ -1681,9 +1681,10 @@ interpretPackageEnv dflags = do
   where
     -- Loading environments (by name or by location)
 
+    archOS = platformArchOS (targetPlatform dflags)
     namedEnvPath :: String -> MaybeT IO FilePath
     namedEnvPath name = do
-     appdir <- versionedAppDir dflags
+     appdir <- versionedAppDir (programName dflags) archOS
      return $ appdir </> "environments" </> name
 
     probeEnvName :: String -> MaybeT IO FilePath
@@ -1720,7 +1721,7 @@ interpretPackageEnv dflags = do
 
     -- e.g. .ghc.environment.x86_64-linux-7.6.3
     localEnvFileName :: FilePath
-    localEnvFileName = ".ghc.environment" <.> versionedFilePath dflags
+    localEnvFileName = ".ghc.environment" <.> versionedFilePath archOS
 
     -- Search for an env file, starting in the current dir and looking upwards.
     -- Fail if we get to the users home dir or the filesystem root. That is,
