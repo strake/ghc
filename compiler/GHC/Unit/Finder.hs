e911252e31d22832cc0a383617b9a9a9fae9fcc1
Wander Hillen <wjw.hillen@gmail.com>
2020-09-25 11:41:06 +0200

    Initial ShortText code and conversion of package db code
    Metric Decrease:
        Naperian
        T10421
        T10421a
        T10547
        T12150
        T12234
        T12425
        T13035
        T18140
        T18304
        T5837
        T6048
        T13253-spj
        T18282
        T18223
        T3064
        T9961
    Metric Increase
        T13701
    
    HFSKJH

--- compiler/GHC/Unit/Finder.hs
+++ compiler/GHC/Unit/Finder.hs
@@ -47,6 +47,7 @@ import GHC.Unit.Finder.Types
 import GHC.Data.FastString
 import GHC.Data.Maybe    ( expectJust )
 
+import qualified GHC.Data.ShortText as ST
 import GHC.Utils.Misc
 import GHC.Utils.Outputable as Outputable
 import GHC.Utils.Panic
@@ -385,7 +386,7 @@ findPackageModule_ hsc_env mod pkg_conf =
 
      mk_hi_loc = mkHiOnlyModLocation dflags package_hisuf
 
-     import_dirs = unitImportDirs pkg_conf
+     import_dirs = ST.unpack <$> unitImportDirs pkg_conf
       -- we never look for a .hi-boot file in an external package;
       -- .hi-boot files only make sense for the home package.
   in
