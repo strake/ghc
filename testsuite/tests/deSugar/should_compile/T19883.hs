89a4181c57e985c3f6dee4393d3582312c06eae6
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2021-06-18 16:27:12 +0200

    Fix desugaring with unboxed types (#19883)

---
+++ testsuite/tests/deSugar/should_compile/T19883.hs
@@ -0,0 +1,20 @@
+{-# Language DataKinds #-}
+{-# Language ExplicitForAll #-}
+{-# Language KindSignatures #-}
+{-# Language NoImplicitPrelude #-}
+{-# Language PolyKinds #-}
+{-# Language RebindableSyntax #-}
+
+module T19883 where
+
+import GHC.Types
+import qualified Prelude
+
+class Eq (a :: TYPE r) where
+  (==) :: a -> a -> Bool
+
+class Num (a :: TYPE r) where
+  fromInteger :: Prelude.Integer -> a
+
+roundDef :: forall (a :: TYPE IntRep) . (Eq a, Num a) => a -> ()
+roundDef 0 = ()
